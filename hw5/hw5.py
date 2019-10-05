913dd21b872f46ba5f2b4fa789d182db23634e83
Christina Lu <luxx0611@umn.edu>
2019-10-05 13:43:44 -0500

    Completed hw5

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -1,6 +1,7 @@
 #a)
 #==========================================
-# Purpose: The function "wizards" takes in three list parameters and tests to see which of the names appears in all three lists.
+# Purpose: The function "wizards" takes in three list
+#           parameters and tests to see which of the names appears in all three lists.
 #The names that appear in all three lists are appended to the wizards_list and the wizards_list is returned.  
 # Input Parameter(s): grades:list of students who get good grades
 #life:list of students who have a life
@@ -20,7 +21,7 @@ def wizards(grades,life,sleep):
 #b)
 #==========================================
 # Purpose: The function "open_slots" takes in one list parameter and tests to see which index in the list is a blank slot.
-#The indexes with blank slots are appended to the index_list and the index_list is returned.  
+#           The indexes with blank slots are appended to the index_list and the index_list is returned.  
 # Input Parameter(s): board:list containing all 9 elements of the board.
 # Return Value(s): index_list,a list of indexes with blank slots 
 #==========================================
@@ -31,8 +32,9 @@ def open_slots(board):
             index_list.append(BruceWayne)
     return index_list
 #==========================================
-# Purpose: The function "winner" takes in one list parameter and tests to see which player one the game by getting three X's or O's in a row, horizontally, vertically or diagonally(or if no one won the game).  
-#Then it returns the winner of the game or indicates if the game isn't over or no one won.  
+# Purpose: The function "winner" takes in one list parameter and tests to see which player one the game by
+#           getting three X's or O's in a row, horizontally, vertically or diagonally(or if no one won the game).  
+#           Then it returns the winner of the game or indicates if the game isn't over or no one won.  
 # Input Parameter(s): board:list containing all 9 elements of the board.
 # Return Value(s): board[0],board[5],board[8]:If someone wins horizontally through the first, second and third rows respectively.
 # board[6],board[7],board[8]: If someone wins vertically through the first, second and third columns respectively.
@@ -42,62 +44,30 @@ def open_slots(board):
 #==========================================
         
 def winner(board):
-    for ClarkKent in board[0]:
-        for Flash in board[1]:
-            if ClarkKent==Flash:
-                for Barry in board[2]:
-                    if Flash==Barry:
-                        return board[0]
-    for OliverQueen in board[3]:
-        for DianaPrince in board[4]:
-            if OliverQueen==DianaPrince:
-                for ArthurCurry in board[5]:
-                    if ArthurCurry==OliverQueen:
-                        return board[5]
-    for VictorStone in board[6]:
-        for Cyborg in board[7]:
-            if VictorStone==Cyborg:
-                for WallyWest in board[8]:
-                    if Cyborg==WallyWest:
-                        return board[8]
-    
-    for Happy in board[0]:
-        for Peter in board[3]:
-            if Happy==Peter:
-                for Ned in board[6]:
-                    if Ned==Peter:
-                        return board[6]
-    for Tony in board[1]:
-        for Pepper in board[4]:
-            if Tony==Pepper:
-                for Morgan in board[7]:
-                    if Pepper==Morgan:
-                        return board[7]
-    for Thanos in board[2]:
-        for Purple in board[5]:
-            if Thanos==Purple:
-                for Stupid in board[8]:
-                    if Thanos==Stupid:
-                        return board[8]
-    for Loki in board[0]:
-        for Dead in board[4]:
-            if Loki==Dead:
-                for Alive in board[8]:
-                    if Loki==Alive:
-                        return board[8] 
-    for Cap in board[2]:
-        for Old_Man in board[4]:
-            if Cap==Old_Man:
-                for Hail_Hydra in board[6]:
-                    if Cap==Hail_Hydra:
-                        return board[6]
+    if board[0]==board[1]==board[2]:
+        return board[0]
+    if board[3]==board[4]==board[5]:
+        return board[5]
+    if board[6]==board[7]==board[8]:
+        return board[8]   
+    if board[0]==board[3]==board[6]:
+        return board[6]
+    if board[1]==board[4]==board[7]:
+        return board[7]
+    if board[2]==board[5]==board[8]:
+        return board[8]
+    if board[0]==board[4]==board[8]:
+        return board[8] 
+    if board[2]==board[4]==board[6]:
+        return board[6]
     for PeterQuillisStupid in board:
         if PeterQuillisStupid=='-':
             return PeterQuillisStupid
     return 'D'
         
 #==========================================
-# Purpose: The function "tic_tac_toe" takes no parameters and creates a random tic tac toe game that the computer plays against itself.  It returns a one character string for the winner, or 'D' for draw.
+# Purpose: The function "tic_tac_toe" takes no parameters and creates a random tic tac toe game that the
+#           computer plays against itself.  It returns a one character string for the winner, or 'D' for draw.
 # Input Parameter(s): None
 # Return Value(s): Victory: Returns 'X' or 'O' strings based on the winner
 # winner(board):The function winner(board) will return 'D' for draw outside the loop
@@ -121,7 +91,8 @@ def tic_tac_toe():
             return Victory
     return winner(board)
      #==========================================
-# Purpose: The function "play_games" takes in the parameter integer n, the number of games to play. It calls the tic_tac_toe function n times and prints the number of times each player wins and the number of times a draw occurs.
+# Purpose: The function "play_games" takes in the parameter integer n, the number of games to play. It calls the tic_tac_toe
+#           function n times and prints the number of times each player wins and the number of times a draw occurs.
 # Input Parameter(s): n: integer, the number of games played
 # Return Value(s): none
 #==========================================
